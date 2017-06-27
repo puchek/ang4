35b4a27080f6ea4b8bd9b2a4fdda001199a4522e
KozlovAV <alekseykozlov@gmail.com>
2017-06-27 18:22:28 +0300

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule} from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+import { ServersService } from './servers/servers.service';
+import { UsersService } from './users/users.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent,
+    ErrorPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
-  providers: [],
+  providers: [ServersService, UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
